e77afa5501f69802032ad783c35208729ce20d50
William Hleucka <william.hleucka@gmail.com>
2023-06-29 07:37:02 -0600

    fix: cleaning things up

--- src/Controllers/Home/HomeController.php
+++ src/Controllers/Home/HomeController.php
@@ -1,37 +1,39 @@
 <?php
 
-namespace Nebula\Controllers;
+namespace Nebula\Controllers\Home;
 
-use StellarRouter\{Get, Post};
+use StellarRouter\Get;
+use Nebula\Controllers\Controller;
 
 class HomeController extends Controller
 {
     #[Get("/", "home.index")]
     public function index(): string
     {
+        // A simple response
         return "hello, world!";
     }
 
     #[Get("/view", "home.view")]
     public function view(): string
     {
+        // To render a twig template, just simply call the twig helper function
         return twig("home/index.html", ["msg" => "hello, world!"]);
     }
 
-    #[Get("/test/{echo}", "home.placeholder")]
-    public function placeholder(string $echo): string
+    #[Get("/echo/{echo}", "home.echo")]
+    public function echo(string $echo): string
     {
         // The placeholder echo is derived from the URI
         // and it is accessible as a method parameter.
         return $echo;
     }
 
-    #[Post("/api/test", "home.test", ["api"])]
+    #[Get("/api/test", "home.test", ["api"])]
     public function test(): int
     {
         // When you use the api middleware, 42 is returned
         // inside of a JSON response automatically.
-        // There is no need to encode.
         return 42;
     }
 }
