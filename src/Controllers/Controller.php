70fcf1ccce9f283331c442b6125a0f2b08773638
William Hleucka <william.hleucka@gmail.com>
2023-07-07 13:50:47 -0600

    fix: controller, remove unwanted stuff

--- src/Controllers/Controller.php
+++ src/Controllers/Controller.php
@@ -2,15 +2,12 @@
 
 namespace Nebula\Controllers;
 
-use Nebula\Container\Container;
 use Nebula\Validation\Validate;
 
 class Controller
 {
-    protected Container $container;
     /**
      * Validate the controller request based on an array of rules
-     * If there is a validation error, then you will recieve null
      * @param array<int,mixed> $rules
      */
     protected function validate(array $rules): bool
