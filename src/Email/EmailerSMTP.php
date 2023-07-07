982e822c0e67aa26f3959d750322d8251fa6bba3
William Hleucka <william.hleucka@gmail.com>
2023-07-07 13:47:43 -0600

    refactor: email class

--- src/Email/EmailerSMTP.php
+++ src/Email/EmailerSMTP.php
@@ -9,7 +9,6 @@ class EmailerSMTP
 {
     private bool $enabled = true;
     private bool $debug = true;
-    private PHPMailer $mail;
     private array $to_addresses = [];
     private array $cc_addresses = [];
     private array $bcc_addresses = [];
@@ -17,7 +16,11 @@ class EmailerSMTP
     private string $subject = "";
     private string $body = "";
     private string $plain = "";
+    private PHPMailer $mail;
 
+    /**
+     * @param array<int,mixed> $config
+     */
     public function __construct(array $config)
     {
         extract($config);
@@ -25,50 +28,21 @@ class EmailerSMTP
         $this->debug = $debug;
         $this->mail = new PHPMailer($debug);
 
-        //Server settings
-        $this->mail->isSMTP();
-        $this->mail->Host = $host;
-        $this->mail->Username = $username;
-        $this->mail->Password = $password;
-        $this->mail->Port = $port;
-        $this->mail->SMTPAuth = true;
-        if ($debug) {
-            $this->mail->SMTPDebug = SMTP::DEBUG_SERVER;
-        }
-        $this->mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+        // Server settings
+        $this->configureServer($host, $username, $password, $port);
 
         // Mail settings
-        $this->mail->setFrom($from_address);
-        $this->mail->addReplyTo($reply_to_address);
-        $this->mail->isHTML(true);
-        $this->mail->CharSet = "UTF-8";
+        $this->configureMail($from_address, $reply_to_address);
     }
 
     public function send(): bool
     {
-        if (
-            empty($this->to_addresses) ||
-            !$this->enabled ||
-            trim($this->body) . trim($this->plain) === ""
-        ) {
+        if (!$this->shouldSend()) {
             return false;
         }
 
-        // Add the addresses
-        foreach ($this->to_addresses as $address) {
-            $this->mail->addAddress($address);
-        }
-        foreach ($this->cc_addresses as $address) {
-            $this->mail->addCC($address);
-        }
-        foreach ($this->bcc_addresses as $address) {
-            $this->mail->addBCC($address);
-        }
-
-        // Attachments
-        foreach ($this->attachments as $attachment) {
-            $this->mail->addAttachment($attachment);
-        }
+        $this->addAddresses();
+        $this->addAttachments();
 
         // Build message
         $this->mail->Subject = $this->subject;
@@ -78,69 +52,123 @@ class EmailerSMTP
         return $this->mail->send();
     }
 
+    /**
+     * @param mixed $addresses
+     */
     public function setTo(...$addresses): EmailerSMTP
     {
-        $this->to_addresses = [];
-        foreach ($addresses as $address) {
-            if (filter_var($address, FILTER_VALIDATE_EMAIL)) {
-                $this->to_addresses[] = $address;
-            }
-        }
+        $this->to_addresses = $this->filterValidEmailAddresses($addresses);
         return $this;
     }
 
+    /**
+     * @param mixed $addresses
+     */
     public function setCC(...$addresses): EmailerSMTP
     {
-        $this->cc_addresses = [];
-        foreach ($addresses as $address) {
-            if (filter_var($address, FILTER_VALIDATE_EMAIL)) {
-                $this->cc_addresses[] = $address;
-            }
-        }
+        $this->cc_addresses = $this->filterValidEmailAddresses($addresses);
         return $this;
     }
 
+    /**
+     * @param mixed $addresses
+     */
     public function setBCC(...$addresses): EmailerSMTP
     {
-        $this->bcc_addresses = [];
-        foreach ($addresses as $address) {
-            if (filter_var($address, FILTER_VALIDATE_EMAIL)) {
-                $this->bcc_addresses[] = $address;
-            }
-        }
+        $this->bcc_addresses = $this->filterValidEmailAddresses($addresses);
         return $this;
     }
 
+    /**
+     * @param mixed $attachments
+     */
     public function setAttachments(...$attachments): EmailerSMTP
     {
-        $this->attachments = [];
-        foreach ($attachments as $attachment) {
-            $this->attachments[] = trim($attachment);
-        }
+        $this->attachments = array_map('trim', $attachments);
         return $this;
     }
 
-    public function setSubject(string $subject): EmailerSMTP
+    public function setSubject(string $subject): self
     {
         $this->subject = trim($subject);
         return $this;
     }
 
-    public function setBody(string $body): EmailerSMTP
+    public function setBody(string $body): self
     {
         $this->body = trim($body);
         return $this;
     }
-
+    /**
+     * @param array<int,mixed> $data
+     */
     public function setTemplate(string $path, array $data = []): EmailerSMTP
     {
         $this->body = twig($path, $data);
         return $this;
     }
 
-    public function setPlain(string $text): EmailerSMTP
+    public function setPlain(string $text): self
     {
         $this->plain = trim($text);
         return $this;
+    }
+
+    private function configureServer(string $host, string $username, string $password, int $port): void
+    {
+        $this->mail->isSMTP();
+        $this->mail->Host = $host;
+        $this->mail->Username = $username;
+        $this->mail->Password = $password;
+        $this->mail->Port = $port;
+        $this->mail->SMTPAuth = true;
+        if ($this->debug) {
+            $this->mail->SMTPDebug = SMTP::DEBUG_SERVER;
+        }
+        $this->mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+    }
+
+    private function configureMail(string $from_address, string $reply_to_address): void
+    {
+        $this->mail->setFrom($from_address);
+        $this->mail->addReplyTo($reply_to_address);
+        $this->mail->isHTML(true);
+        $this->mail->CharSet = "UTF-8";
+    }
+
+    private function shouldSend(): bool
+    {
+        return (
+            !empty($this->to_addresses) &&
+            $this->enabled &&
+            trim($this->body) . trim($this->plain) !== ""
+        );
+    }
+
+    private function addAddresses(): void
+    {
+        foreach ($this->to_addresses as $address) {
+            $this->mail->addAddress($address);
+        }
+        foreach ($this->cc_addresses as $address) {
+            $this->mail->addCC($address);
+        }
+        foreach ($this->bcc_addresses as $address) {
+            $this->mail->addBCC($address);
+        }
+    }
+
+    private function addAttachments(): void
+    {
+        foreach ($this->attachments as $attachment) {
+            $this->mail->addAttachment($attachment);
+        }
+    }
+    /**
+     * @param array<int,mixed> $addresses
+     */
+    private function filterValidEmailAddresses(array $addresses): array
+    {
+        return array_filter($addresses, fn($address) => filter_var($address, FILTER_VALIDATE_EMAIL));
     }
 }
