e153254ff9c00388f9bc8aa0f6856baf284ba0a0
William Hleucka <william.hleucka@gmail.com>
2023-07-08 11:38:17 -0600

    update: pub

--- public/index.php
+++ public/index.php
@@ -1,9 +1,14 @@
 <?php
 /**
- * NEBULA -- a powerful PHP framework inspired by the cosmos
+ * NEBULA -- a powerful PHP framework
  * Github: https://github.com/libra-php/nebula
  * Created: william.hleucka@gmail.com
  * License: MIT
  */
 require_once "bootstrap.php";
+
+// Run the app using attribute-based routing
 app()->run();
+
+// Or, you can define the routes and call run
+//app()->get("/", payload: fn() => "hello, world")->run();
