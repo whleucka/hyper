56a638ec185877e76d499361f72acdcf4aa67fe7
William Hleucka <william.hleucka@gmail.com>
2023-07-26 12:15:07 -0600

    refactor 👍

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,27 +2,57 @@
 
 namespace App\Http;
 
+use Nebula\Framework\Application;
 use Nebula\Interfaces\Http\Request;
-use Nebula\Http\Response;
+use Nebula\Interfaces\Http\Response;
 use Nebula\Interfaces\System\Kernel as NebulaKernel;
 use Throwable;
 
 class Kernel implements NebulaKernel
 {
-    public function handleRequest(Request $request): Response
+    private Application $app;
+
+    /**
+     * Setup the application
+     */
+    public function setup(Application $app): Kernel
     {
-      $response = new Response;
-      $response->setStatusCode(200);
-      $response->setContent("hello, world");
-      return $response;
+        $this->app = $app;
+        $this->registerInterfaces();
+        return $this;
     }
 
+    /**
+     * Register all default the framework interface binding
+     */
+    public function registerInterfaces(): void
+    {
+        $this->app->bind(\Nebula\Interfaces\Http\Response::class, \Nebula\Http\Response::class);
+    }
+
+    /**
+     * Handle the request and return a response
+     */
+    public function handleRequest(Request $request): Response
+    {
+        $response = $this->app->get(Response::class);
+        $response->setStatusCode(200);
+        $response->setContent("hello, world");
+        return $response;
+    }
+
+    /**
+     * Handle any application exceptions
+     */
     public function handleException(Throwable $exception): void
     {
     }
 
+    /**
+     * Terminate the application
+     */
     public function terminate(): void
     {
-      exit;
+        exit;
     }
 }
--- src/Framework/Application.php
+++ src/Framework/Application.php
@@ -13,13 +13,14 @@ class Application extends Container
   public function run(string $class): void
   {
     $this->kernel = $this->get($class);
-    $request = $this->get(Request::class);
-    $this->execute($request);
+    $this->kernel->setup($this);
+    $this->execute();
   }
 
-  public function execute(Request $request): void
+  public function execute(): void
   {
-      $this->kernel->handleRequest($request)->send();
-      $this->kernel->terminate();
+    $request = $this->get(Request::class);
+    $this->kernel->handleRequest($request)->send();
+    $this->kernel->terminate();
   }
 }
