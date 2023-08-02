34c1779879b5daab56c77dcb334312c167a9f55b
William Hleucka <william.hleucka@gmail.com>
2023-08-02 07:53:35 -0600

    feat: css script (build tailwindcss); update tailwind theme; register / sign in templates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,48 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./views/**/*.{html,twig}"],
-  theme: {
-    extend: {},
+export const content = ["./views/**/*.{html,twig}"];
+export const darkMode = 'class';
+export const theme = {
+  extend: {
+    colors: {
+      primary: { "50": "#eef2ff", "100": "#e0e7ff", "200": "#c7d2fe", "300": "#a5b4fc", "400": "#818cf8", "500": "#6366f1", "600": "#4f46e5", "700": "#4338ca", "800": "#3730a3", "900": "#312e81", "950": "#1e1b4b" }
+    }
   },
-  plugins: [],
-}
+  fontFamily: {
+    'body': [
+      'Nunito Sans',
+      'ui-sans-serif',
+      'system-ui',
+      '-apple-system',
+      'system-ui',
+      'Segoe UI',
+      'Roboto',
+      'Helvetica Neue',
+      'Arial',
+      'Noto Sans',
+      'sans-serif',
+      'Apple Color Emoji',
+      'Segoe UI Emoji',
+      'Segoe UI Symbol',
+      'Noto Color Emoji'
+    ],
+    'sans': [
+      'Nunito Sans',
+      'ui-sans-serif',
+      'system-ui',
+      '-apple-system',
+      'system-ui',
+      'Segoe UI',
+      'Roboto',
+      'Helvetica Neue',
+      'Arial',
+      'Noto Sans',
+      'sans-serif',
+      'Apple Color Emoji',
+      'Segoe UI Emoji',
+      'Segoe UI Symbol',
+      'Noto Color Emoji'
+    ]
+  }
+};
+export const plugins = [];
 
