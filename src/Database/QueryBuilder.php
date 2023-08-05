0014e1a638f13ed4039777d25468a09b3d4e89b3
William Hleucka <william.hleucka@gmail.com>
2023-08-04 22:08:50 -0600

    fix: refactor qb

--- src/Database/QueryBuilder.php
+++ src/Database/QueryBuilder.php
@@ -2,104 +2,169 @@
 
 namespace Nebula\Database;
 
+use Closure;
 use Nebula\Interfaces\Database\QueryBuilder as QueryBuilderInterface;
 use Nebula\Interfaces\Model\Model;
 
 class QueryBuilder implements QueryBuilderInterface
 {
-    private string $sql = "";
+    private $mode = "select";
+    private string $table_name = "";
     private array $values = [];
+    private array $columns = [];
+    private array $where = [];
+    private array $having = [];
+    private array $group_by = [];
+    private array $order_by = [];
+    private ?int $limit = null;
+    private ?int $offset = null;
 
     public function values(): array
     {
         return $this->values;
     }
 
+    /**
+     * @param array<int,mixed> $data
+     * @param Closure(): void $fn
+     * @param string $separator
+     */
+    private function mapToString(array $data, \Closure $fn, string $separator = ", "): string
+    {
+        $columns = array_map($fn, $data);
+        return implode($separator, $columns);
+    }
+
     public static function select(Model $model): self
     {
         $qb = new QueryBuilder();
-        $qb->sql = "SELECT * FROM $model->table_name ";
+        $qb->table_name = $model->table_name;
+        $qb->mode = "select";
         return $qb;
     }
 
     public static function insert(Model $model): self
     {
         $qb = new QueryBuilder();
-        $qb->sql = "INSERT INTO $model->table_name SET ";
+        $qb->table_name = $model->table_name;
+        $qb->mode = "insert";
         return $qb;
     }
 
     public static function update(Model $model): self
     {
         $qb = new QueryBuilder();
-        $qb->sql = "UPDATE $model->table_name SET ";
+        $qb->table_name = $model->table_name;
+        $qb->mode = "update";
         return $qb;
     }
 
     public static function delete(Model $model): self
     {
         $qb = new QueryBuilder();
-        $qb->sql = "DELETE FROM $model->table_name ";
+        $qb->table_name = $model->table_name;
+        $qb->mode = "delete";
         return $qb;
     }
 
+    /**
+     * @param array<int,mixed> $columns
+     */
     public function columns(array $columns): self
     {
-        $columns = implode(', ', $columns);
-        $this->sql = str_replace('*', $columns, $this->sql);
+        if ($this->mode === "insert" || $this->mode === "update") {
+            $this->values = array_merge($this->values, array_values($columns));
+        }
+        $this->columns = $columns;
         return $this;
     }
 
-    private function mapToString(array $data, \Closure $fn, $separator = ", "): string
+    public function where(array $where): self
     {
-        $columns = array_map($fn, $data);
-        return implode($separator, $columns);
-    }
-
-    public function where(array $columns): self
-    {
-        $having_clause = $this->mapToString(array_keys($columns), fn ($key) => "($key = ?)", " AND ");
-        $this->sql .= "WHERE $having_clause ";
-        $this->values = array_merge($this->values, array_values($columns));
+        $this->where = $where;
+        $this->values = array_merge($this->values, array_values($where));
         return $this;
     }
 
-    public function having(array $columns): self
+    public function groupBy(array $group_by): self
     {
-        $having_clause = $this->mapToString(array_keys($columns), fn ($key) => "($key = ?)", " AND ");
-        $this->sql .= "HAVING $having_clause ";
-        $this->values = array_merge($this->values, array_values($columns));
+        $this->group_by = $group_by;
         return $this;
     }
 
-    public function groupBy(array $columns): self
+    public function having(array $having): self
     {
-        $group_by_clause = $this->mapToString($columns, fn ($column) => $column, ", ");
-        $this->sql .= "GROUP BY $group_by_clause ";
+        $this->having = $having;
+        $this->values = array_merge($this->values, array_values($having));
         return $this;
     }
 
-    public function orderBy(array $columns): self
+
+    public function orderBy(array $order_by): self
     {
-        $order_by_clause = implode(", ", array_map(fn ($key, $value) => "$key $value", array_keys($columns), array_values($columns)));
-        $this->sql .= "ORDER BY $order_by_clause ";
+        $this->order_by = $order_by;
         return $this;
     }
 
     public function limit(int $limit): self
     {
-        $this->sql .= "LIMIT $limit";
+        $this->limit = $limit;
         return $this;
     }
 
     public function offset(int $offset): self
     {
-        $this->sql .= ", $offset";
+        $this->offset = $offset;
         return $this;
     }
 
     public function build(): string
     {
-        return $this->sql;
+        $sql = match ($this->mode) {
+            "select" => "SELECT * FROM $this->table_name ",
+            "insert" => "INSERT INTO $this->table_name SET ",
+            "update" => "UPDATE $this->table_name SET ",
+            "delete" => "DELETE FROM $this->table_name ",
+        };
+        if ($this->mode === "select") {
+            if (!empty($this->columns)) {
+                $select_stmt = implode(', ', $this->columns);
+                $sql = str_replace('*', $select_stmt, $sql);
+            }
+            if (!empty($this->where)) {
+                $where_clause = $this->mapToString(array_keys($this->where), fn ($key) => "($key = ?)", " AND ");
+                $sql .= "WHERE $where_clause ";
+            }
+            if (!empty($this->group_by)) {
+                $group_by_clause = $this->mapToString($this->group_by, fn ($column) => $column, ", ");
+                $sql .= "GROUP BY $group_by_clause ";
+            }
+            if (!empty($this->having)) {
+                $having_clause = $this->mapToString(array_values($this->group_by), fn ($key) => "($key = ?)", " AND ");
+                $sql .= "HAVING $having_clause ";
+            }
+            if (!empty($this->order_by)) {
+                $order_by_clause = implode(", ", array_map(fn ($key, $value) => "$key $value", array_keys($this->order_by), array_values($this->order_by)));
+                $sql .= "ORDER BY $order_by_clause ";
+            }
+            if (!is_null($this->limit)) {
+                $sql .= "LIMIT $this->limit";
+            }
+            if (!is_null($this->offset)) {
+                $sql .= ", $this->offset";
+            }
+        } else {
+            if (!empty($this->columns)) {
+                $columns = $this->mapToString(array_keys($this->columns), fn ($key) => "$key = ?", ", ");
+                $sql .= "$columns ";
+            }
+        }
+        if ($this->mode === "update" || $this->mode === "delete") {
+            if (!empty($this->where)) {
+                $where_clause = $this->mapToString(array_keys($this->where), fn ($key) => "($key = ?)", " AND ");
+                $sql .= "WHERE $where_clause ";
+            }
+        }
+        return trim($sql);
     }
 }
--- tests/Database/QueryBuilderTest.php
+++ tests/Database/QueryBuilderTest.php
@@ -13,14 +13,39 @@ final class QueryBuilderTest extends TestCase
   public function test_select_query(): void
   {
     $qb = QueryBuilder::select(new User)
-    ->columns(["id", "email", "username"])
-    ->where(["id" => 1, "username" => "test"])
-    ->having(["id" => 2, "username" => "blue"])
-    ->groupBy(["id", "username"])
-    ->orderBy(["id" => "ASC", "username" => "DESC"])
+    ->columns(["id", "email", "name"])
+    ->where(["id" => 1, "name" => "test"])
+    ->groupBy(["id", "name"])
+    ->having(["id" => 2, "name" => "blue"])
+    ->orderBy(["id" => "ASC", "name" => "DESC"])
     ->limit(1)
     ->offset(2);
     $this->assertSame($qb->values(), [1, "test", 2, "blue"]);
-    $this->assertSame("SELECT id, email, username FROM users WHERE (id = ?) AND (username = ?) HAVING (id = ?) AND (username = ?) GROUP BY id, username ORDER BY id ASC, username DESC LIMIT 1, 2", $qb->build());
+    $this->assertSame("SELECT id, email, name FROM users WHERE (id = ?) AND (name = ?) GROUP BY id, name HAVING (id = ?) AND (name = ?) ORDER BY id ASC, name DESC LIMIT 1, 2", $qb->build());
+  }
+
+  public function test_insert_query(): void
+  {
+    $qb = QueryBuilder::insert(new User)
+    ->columns(["name" => "test", "email" => "test@test.com"]);
+    $this->assertSame($qb->values(), ["test", "test@test.com"]);
+    $this->assertSame("INSERT INTO users SET name = ?, email = ?", $qb->build());
+  }
+
+  public function test_update_query(): void
+  {
+    $qb = QueryBuilder::update(new User)
+    ->columns(["name" => "test", "email" => "test@test.com"])
+    ->where(["id" => 1]);
+    $this->assertSame($qb->values(), ["test", "test@test.com", 1]);
+    $this->assertSame("UPDATE users SET name = ?, email = ? WHERE (id = ?)", $qb->build());
+  }
+
+  public function test_delete_query(): void
+  {
+    $qb = QueryBuilder::delete(new User)
+    ->where(["id" => 1]);
+    $this->assertSame($qb->values(), [1]);
+    $this->assertSame("DELETE FROM users WHERE (id = ?)", $qb->build());
   }
 }
