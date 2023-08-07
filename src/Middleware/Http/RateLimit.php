dafd19be5fcdcf37eec79012318503d49ad1f9ac
William Hleucka <william.hleucka@gmail.com>
2023-08-06 19:04:16 -0600

    fix: spacing

--- src/Middleware/Http/RateLimit.php
+++ src/Middleware/Http/RateLimit.php
@@ -8,48 +8,48 @@ use Closure;
 
 class RateLimit implements Middleware
 {
-    public function handle(Request $request, Closure $next): Response
-    {
-        if (env("REDIS_ENABLED")) {
-            $result = $this->rateLimit();
-            if (!$result) {
-                $response = app()->get(Response::class);
-                $response->setStatusCode(429);
-                $response->setContent('Rate limit exceeded');
-                return $response;
-            }
-        }
-        $response = $next($request);
-
+  public function handle(Request $request, Closure $next): Response
+  {
+    if (env("REDIS_ENABLED")) {
+      $result = $this->rateLimit();
+      if (!$result) {
+        $response = app()->get(Response::class);
+        $response->setStatusCode(429);
+        $response->setContent('Rate limit exceeded');
         return $response;
+      }
+    }
+    $response = $next($request);
+
+    return $response;
+  }
+
+  private function rateLimit(): bool
+  {
+    $config = config('redis');
+    $client = new \Predis\Client($config);
+
+    $ipAddress = requestIp();
+
+    $rateLimit = 25; // Number of requests allowed per minute
+    $ipKey = "ip:$ipAddress";
+
+    // Add the current timestamp to the Redis Sorted Set
+    $timestamp = time();
+    $client->zadd($ipKey, $timestamp, $timestamp);
+
+    // Remove any timestamps that exceed the rate limit window
+    $windowStart = $timestamp - 60; // 60 seconds = 1 minute window
+    $client->zremrangebyscore($ipKey, 0, $windowStart);
+
+    // Get the number of requests made from the IP address in the window
+    $requestsInWindow = $client->zcard($ipKey);
+
+    if ($requestsInWindow > $rateLimit) {
+      // IP address has exceeded the rate limit, return an error response
+      return false;
     }
 
-    private function rateLimit(): bool
-    {
-        $config = config('redis');
-        $client = new \Predis\Client($config);
-
-        $ipAddress = requestIp();
-
-        $rateLimit = 25; // Number of requests allowed per minute
-        $ipKey = "ip:$ipAddress";
-
-        // Add the current timestamp to the Redis Sorted Set
-        $timestamp = time();
-        $client->zadd($ipKey, $timestamp, $timestamp);
-
-        // Remove any timestamps that exceed the rate limit window
-        $windowStart = $timestamp - 60; // 60 seconds = 1 minute window
-        $client->zremrangebyscore($ipKey, 0, $windowStart);
-
-        // Get the number of requests made from the IP address in the window
-        $requestsInWindow = $client->zcard($ipKey);
-
-        if ($requestsInWindow > $rateLimit) {
-            // IP address has exceeded the rate limit, return an error response
-            return false;
-        }
-
-        return true;
-    }
+    return true;
+  }
 }
