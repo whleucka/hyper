d9f6655c3b74f2bbbc730f7dab17c253c2f70d6e
William Hleucka <william.hleucka@gmail.com>
2023-08-07 10:50:45 -0600

    fix: log depends on env setting

--- src/Middleware/Http/Log.php
+++ src/Middleware/Http/Log.php
@@ -15,8 +15,7 @@ class Log implements Middleware
 {
     public function handle(Request $request, Closure $next): Response
     {
-        $route_middleware = $request->route->getMiddleware();
-        if (in_array("log", $route_middleware) && env("LOG_ENABLED")) {
+        if (env("LOG_ENABLED")) {
             $this->logRequest($request);
         }
 
