2454aa89e68c6067634ca5034ff61ce7d6d840d7
William Hleucka <william.hleucka@gmail.com>
2023-08-06 19:07:27 -0600

    feat: sign out

---
+++ app/Controllers/Admin/Auth/SignOutController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controllers\Admin\Auth;
+
+use Nebula\Controller\Controller;
+use StellarRouter\{Get, Group};
+
+#[Group(prefix: "/admin")]
+final class SignOutController extends Controller
+{
+  #[Get("/sign-out", "sign-in.index")]
+  public function index(): mixed
+  {
+    session()->destroy();
+    return redirectRoute("sign-in.index");
+  }
+}
