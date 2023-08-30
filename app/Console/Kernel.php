dc77fe9a2341d76b8ee8849cb22e0d48e70649eb
William Hleucka <william.hleucka@gmail.com>
2023-08-30 09:20:58 -0600

    revert: kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,7 +3,6 @@
 namespace App\Console;
 
 use Nebula\Console\Kernel as ConsoleKernel;
-use App\Soprano\Music\Scan;
 
 final class Kernel extends ConsoleKernel
 {
@@ -16,12 +15,6 @@ final class Kernel extends ConsoleKernel
     // https://www.php.net/manual/en/function.getopt.php
     $this->registerCommand('short', 'v', "Display version", fn() => $this->version());
     $this->registerCommand('long', 'version', "Display version", fn() => $this->version());
-
-    $this->registerCommand('long', 'scan-music', "Scan music library. Usage scan-music=/music/directory", function(string $directory) {
-      print_r($directory);die;
-      // $scanner = new Scan();
-      // print_r($scanner->findFiles($directory));
-    });
   }
 
   public function version(): void
