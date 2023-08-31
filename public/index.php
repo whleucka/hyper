2626058b34667c00d0e1d15753b1cb935747a216
William Hleucka <william.hleucka@gmail.com>
2023-08-30 19:13:57 -0600

    fix: change base layout

--- public/index.php
+++ public/index.php
@@ -14,15 +14,7 @@ $app = require_once __DIR__ . "/../bootstrap/app.php";
 // Attribute-based-routing
 // Example / web endpoint
 $app->route('GET', '/', function() {
-    $count = session()->get('count') ?? 0;
-    return twig('welcome/index.html', ['count' => $count]);
+    return twig('welcome/index.html');
 });
-// Example /api/test api endpoint
-$app->route('POST', '/api/count', function() {
-    $count = session()->get('count');
-    $count++;
-    session()->set('count', $count);
-    return $count;
-}, middleware: ['api']);
 
 $app->run(Nebula\Interfaces\Http\Kernel::class);
