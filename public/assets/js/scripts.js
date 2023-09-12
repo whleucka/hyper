f7a803d072f831c0c51d1a10670df4e144c215fe
William Hleucka <william.hleucka@gmail.com>
2023-09-12 13:02:44 -0600

    fix: allow delete char

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -9,15 +9,13 @@ const keypadClick = (e) => {
   const submit = document.getElementById("two-factor-submit");
   const character = e.currentTarget.value;
   const value = code_input.value;
-  if (value.length < 6) {
-    if (isNaN(character)) {
-      if (character === 'bs') {
-        code_input.value = value.slice(0, -1);
-      } else if (character === 'ent') {
-        submit.click();
-      }
-    } else {
-      code_input.value = value + character; 
+  if (isNaN(character)) {
+    if (character === 'bs') {
+      code_input.value = value.slice(0, -1);
+    } else if (character === 'ent') {
+      submit.click();
     }
+  } else if (value.length < 6) {
+    code_input.value = value + character; 
   }
 }
