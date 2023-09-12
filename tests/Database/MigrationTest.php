d89104be6ebe88de8744ccdd3aec0f9201230f33
William Hleucka <william.hleucka@gmail.com>
2023-09-12 09:37:14 -0600

    fix: mig test

--- tests/Database/MigrationTest.php
+++ tests/Database/MigrationTest.php
@@ -19,8 +19,7 @@ final class MigrationTest extends TestCase
     public function test_migration_up_query(): void
     {
         $migration = $this->migration->up();
-        $query =
-            "CREATE TABLE IF NOT EXISTS users (id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT, uuid CHAR(36) NOT NULL DEFAULT (UUID()), name VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, password BINARY(96) NOT NULL, two_fa_secret CHAR(16), remember_token CHAR(32), reset_token CHAR(32), reset_expires_at BIGINT UNSIGNED, failed_login_attempts TINYINT UNSIGNED NOT NULL DEFAULT 0, lock_expires_at BIGINT UNSIGNED, created_at DATETIME(0) NOT NULL DEFAULT CURRENT_TIMESTAMP, updated_at TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP, UNIQUE KEY (email), PRIMARY KEY (id))";
+        $query = "CREATE TABLE IF NOT EXISTS users (id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT, uuid CHAR(36) NOT NULL DEFAULT (UUID()), name VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, password BINARY(96) NOT NULL, two_fa_secret CHAR(16), remember_token CHAR(64), reset_token CHAR(64), reset_expires_at BIGINT UNSIGNED, failed_login_attempts TINYINT UNSIGNED NOT NULL DEFAULT 0, lock_expires_at BIGINT UNSIGNED, created_at DATETIME(0) NOT NULL DEFAULT CURRENT_TIMESTAMP, updated_at TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP, UNIQUE KEY (email), PRIMARY KEY (id))";
         $this->assertSame($query, $migration);
     }
 
