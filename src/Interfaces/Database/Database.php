8021a3c92326601641c4683b9c99c68dcb0e5ce7
William Hleucka <william.hleucka@gmail.com>
2023-09-22 07:51:19 -0600

    fix: add missing interface methods Database

--- src/Interfaces/Database/Database.php
+++ src/Interfaces/Database/Database.php
@@ -7,4 +7,7 @@ interface Database
 {
     public function connect(array $config): void;
     public function run(string $sql, array $params = []): ?PDOStatement;
+    public function select(string $sql, ...$params): mixed;
+    public function selectAll(string $sql, ...$params): ?array;
+    public function query(string $sql, ...$params): ?PDOStatement;
 }
