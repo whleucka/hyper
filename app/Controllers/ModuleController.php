b0c9931de1bd6a135a0e6486f6ecc2c1e1feda35
William Hleucka <william.hleucka@gmail.com>
2023-09-25 14:48:46 -0600

    fix: refactor dashboard route

--- app/Controllers/ModuleController.php
+++ app/Controllers/ModuleController.php
@@ -5,7 +5,7 @@ namespace App\Controllers;
 use Nebula\Controller\ModuleController as NebulaModule;
 use StellarRouter\{Get, Group};
 
-#[Group(prefix: "/module", middleware: ["auth"])]
+#[Group(middleware: ["auth"])]
 class ModuleController extends NebulaModule
 {
     #[Get("/dashboard", "dashboard.index")]
