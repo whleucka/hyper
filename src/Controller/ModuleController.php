307b2c0df1e9ed51febdbbc2eec29c5d9d220233
William Hleucka <william.hleucka@gmail.com>
2023-09-25 13:41:21 -0600

    fix: module controller extends controller

--- src/Controller/ModuleController.php
+++ src/Controller/ModuleController.php
@@ -4,7 +4,7 @@ namespace Nebula\Controller;
 
 use Nebula\Interfaces\Controller\Controller as NebulaController;
 
-class ModuleController implements NebulaController
+class ModuleController extends Controller implements NebulaController
 {
     public function __construct()
     {
